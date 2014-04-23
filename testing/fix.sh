efa663cfac190640a62901972a3bfd68831a27b7
Foggalong <joshua.h.fogg@gmail.com>
2014-04-23 11:36:29 +0100

    Update to testing version

--- testing/fix.sh
+++ testing/fix.sh
@@ -19,7 +19,7 @@ if [[ $UID -ne 0 ]]; then
 fi
 
 # Deals with the flags
-while getopts "fuhv" arg; do
+while getopts "hv" arg; do
 case $arg in
     # u) mode="u";; <-- potential for unfixing mode 
     h) echo -e "This is the sample help screen."
@@ -30,40 +30,53 @@ case $arg in
     esac
 done
 
-echo $mode
+# Downloads icon data from GitHub to /tmp/
+wget -O /tmp/test.txt https://raw.githubusercontent.com/Foggalong/hardcode-fixer/master/testing/test.txt
 
 while read line; do
 	# Splits line into array
 	IFS="|" read -a array <<< $line
 	# Readability renaming
 	name=$(echo ${array[1]} | sed -e "s/\r//g")
-	launcher="$name.desktop"
+	launcher="${name}.desktop"
 	current=$(echo ${array[2]} | sed -e "s/\r//g")
 	newicon=$(echo ${array[3]} | sed -e "s/\r//g")
+	#
+	# Problem with spec. chars. in file names solved by
+	# creating a seperate var. that escapes them
+	oldicon=$current
+    oldicon="${oldicon//\\/\\\\}" # <- escape all backslashes first
+    oldicon="${oldicon//\//\\/}"  # <- escape slashes
+	#
 	# Local Launchers
-	if [ -f "$HOME/.local/share/applications/$launcher" ]
+	if [ -f "$HOME/.local/share/applications/${launcher}" ]
 	then
-		if [ $current != "steam" ]
+		if [ "${current}" != "steam" ]
 		then
-			echo "Fixing $name..."
-			cp "$current" "$HOME/.local/share/icons/hicolor/48x48/apps/$newicon"
-			sed -i "s/$current/Icon=\ $newicon/g" "$HOME/.local/share/applications/$launcher"
+			if [ -f "$current" ]
+			then
+				echo "L: Fixing $name..."
+				cp "$current" "$HOME/.local/share/icons/hicolor/48x48/apps/"
+				sed -i "s/${oldicon}/${newicon}/g" "$HOME/.local/share/applications/${launcher}"
+			fi
 		else
-			echo "Fixing $name (steam)..."
-			cp "/usr/share/icons/hicolor/48x48/apps/steam.png" "$HOME/.local/share/icons/hicolor/48x48/apps/$newicon"
-			sed -i "s/Icon=steam/Icon=\ $newicon/g" "$HOME/.local/share/applications/$launcher"
-			sed -i "s/= /=/g" "$HOME/.local/share/applications/$launcher" # <- dirty fix for space bug
+			echo "L: Fixing $name (steam)..."
+			cp "/usr/share/icons/hicolor/48x48/apps/steam.png" "$HOME/.local/share/icons/hicolor/48x48/apps/${newicon}.png"
+			sed -i "s/Icon=steam/Icon=${newicon}/g" "$HOME/.local/share/applications/${launcher}"
+		fi
+	else
+		: #pass 
+	fi
+	# Global Launchers
+	if [ -f "/usr/share/applications/${launcher}" ]
+	then
+		if [ -f "$current" ]
+		then
+			echo "G: Fixing $name..."
+			cp "$current" "/usr/share/icons/hicolor/48x48/apps/"
+			sed -i "s/${oldicon}/${newicon}/g" "/usr/share/applications/${launcher}"
 		fi
 	else
 		: #pass
 	fi
-	# Global Launchers
-	if [ -f "/usr/share/applications/$launcher" ]
-	then
-		echo "Fixing $name..."
-		cp "$current" "/usr/share/share/icons/hicolor/48x48/apps/$newicon"
-		sed -i "s/$current/Icon=\ $newicon/g" "/usr/share/icons/share/applications/$launcher"
-	else
-		: #pass
-	fi
-done < test.txt
+done < /tmp/test.txt
