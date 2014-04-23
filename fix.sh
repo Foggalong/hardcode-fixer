23016cee9990f310a3a06f904aa7044e062d693a
Nathanel Titane <nathanel.titane@gmail.com>
2014-04-23 17:44:37 -0400

    Update to fix.sh
    Bash standards compliance (quotes, blocks, etc.)
    Comment cleanup
    Port data directory string to its own variable (less clobber)
    Add GUI launch error timeout
    Add data directory creation/existence verification before launching fix

--- fix.sh
+++ fix.sh
@@ -1,46 +1,55 @@
 #!/bin/bash
 
 # Base script for the safe hardcoded icon fixer.
-# Copyright (C) 2014  Joshua Fogg
+# Copyright (C) 2014 Joshua Fogg
+
+# Edited by Nathanel Titane (nathanel.titane@gmail.com)
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License (version 3+) as 
 # published by the Free Software Foundation. You should have received
-# a copy of the GNU General Public License along with this program.  
+# a copy of the GNU General Public License along with this program. 
 # If not, see <http://www.gnu.org/licenses/>.
 
 # Version
-version="0.3.1"
+version="0.3.2"
 
-# The script must run as root.
+# Data directory
+data_dir="$HOME/.local/share/data/hcf/"
+
+# The script must be run as root
 if [[ $UID -ne 0 ]]; then
-	echo "$0 must be run as root"
+	echo "$0: This script must be run as root."
+	sleep 3 # Enables error timeout when script is launched via 'Run in Terminal' command.
 	exit 1
 fi
 
 # Deals with the flags
 while getopts "hv" arg; do
 case $arg in
-    # u) mode="u";; <-- potential for unfixing mode 
-    h) echo -e "This is the sample help screen."
-       echo -e "way for helpfulness!"
-       exit 0;;
-    v) echo "$(basename -- "$0") $version"
-       exit 0
-    esac
+#	u) echo -e "Undo changes." # Eventual 'Undo' selection
+	h) echo -e "This is the sample help screen."
+		echo -e "way for helpfulness!"
+		exit 0;;
+	v) echo "$(basename -- $0) $version"
+		exit 0
+	esac
 done
 
-# Creates data dir & file
-if [ -f "$HOME/.local/share/data/hcf/" ]
+# Creates data directory & file
+if [ -f "$data_dir" ]
 then
-	: #pass
+	: # pass
 else
-	mkdir $HOME/.local/share/data/hcf/
-	touch $HOME/.local/share/data/hcf/fixed.txt
+	mkdir "$data_dir"
+	touch "$data_dir/fixed.txt"
 fi
 
-# Downloads icon data from GitHub to data directory
-wget -O $HOME/.local/share/data/hcf/tofix.txt https://raw.githubusercontent.com/Foggalong/hardcode-fixer/master/data/tofix.txt
+# Verify data directory creation and existence by entering command directory
+cd "$data_dir" || echo "$0: Data directory does not exist or was not created." && exit 1
+
+# Downloads icon data from GitHub repository to data directory
+wget -O "$data_dir/tofix.txt" 'https://raw.githubusercontent.com/Foggalong/hardcode-fixer/master/data/tofix.txt'
 
 while read line; do
 	# Splits line into array
@@ -50,55 +59,52 @@ while read line; do
 	launcher=$(echo ${array[2]}.desktop | sed -e "s/\r//g")
 	current=$(echo ${array[3]} | sed -e "s/\r//g")
 	newicon=$(echo ${array[4]} | sed -e "s/\r//g")
-	#
-	# Problem with spec. chars. in file names solved by
-	# creating a seperate var. that escapes them
-	oldicon=$current
-	oldicon="${oldicon//\\/\\\\}" # <- escape all backslashes first
-	oldicon="${oldicon//\//\\/}"  # <- escape slashes
-	#
+
+	# Escape non-standard and special characters in file names by creating new variable
+	oldicon="$current"
+	oldicon="${oldicon//\\/\\\\}" # escape all backslashes first
+	oldicon="${oldicon//\//\\/}" # escape slashes
+
 	# Local Launchers
 	if [ -f "$HOME/.local/share/applications/${launcher}" ]
 	then
-		if grep -Fxq "$name" $HOME/.local/share/data/hcf/fixed.txt
-		# ^ checks if already fixed
+		if grep -Fxq "$name" "$data_dir/fixed.txt" # checks if already fixed
 		then
-		    : # pass
+			: # pass
 		else
 			if [ "${current}" != "steam" ]
 			then
-				if [ -f "$current" ]
-				# ^ check if icon exists to copy
+				if [ -f "$current" ] # checks if icon exists to copy
 				then
 					echo "L: Fixing $name..."
 					cp "$current" "$HOME/.local/share/icons/hicolor/48x48/apps/"
 					sed -i "s/${oldicon}/${newicon}/g" "$HOME/.local/share/applications/${launcher}"
-					echo $name >> $HOME/.local/share/data/hcf/fixed.txt
+					echo "$name" >> "$data_dir/fixed.txt"
 				fi
 			else
 				echo "L: Fixing $name (steam)..."
 				cp "/usr/share/icons/hicolor/48x48/apps/steam.png" "$HOME/.local/share/icons/hicolor/48x48/apps/${newicon}.png"
 				sed -i "s/Icon=steam/Icon=${newicon}/g" "$HOME/.local/share/applications/${launcher}"
-				echo $name >> $HOME/.local/share/data/hcf/fixed.txt
+				echo "$name" >> "$data_dir/fixed.txt"
 			fi
 		fi
 	fi
+
 	# Global Launchers
 	if [ -f "/usr/share/applications/${launcher}" ]
 	then
-		if grep -Fxq "$name" $HOME/.local/share/data/hcf/fixed.txt
-		# ^ checks if already fixed
+		if grep -Fxq "$name" $data_dir/fixed.txt # checks if already fixed
 		then
-    		: # pass
+			: # pass
 		else
-			if [ -f "$current" ]
-			# ^ check if icon exists to copy
+			if [ -f "$current" ] # checks if icon exists to copy
 			then
 				echo "G: Fixing $name..."
 				cp "$current" "/usr/share/icons/hicolor/48x48/apps/"
 				sed -i "s/${oldicon}/${newicon}/g" "/usr/share/applications/${launcher}"
-				echo $name >> $HOME/.local/share/data/hcf/fixed.txt
+				echo "$name" >> "$data_dir/fixed.txt"
 			fi
 		fi
 	fi
-done < $HOME/.local/share/data/hcf/tofix.txt
+
+done < "$data_dir/tofix.txt"
