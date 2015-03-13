7bec3ed96558ebed673df9f46d0736e821e3ccf9
hlechner <hlechner@gmail.com>
2015-03-13 19:20:17 -0300

    Updated the date version info

--- fix.sh
+++ fix.sh
@@ -11,7 +11,7 @@
 # If not, see <http://www.gnu.org/licenses/>.
 
 # Version info
-date=201411230  # [year][month][date][extra]
+date=201503130  # [year][month][date][extra]
 
 # Locations
 git_locate="https://raw.githubusercontent.com/Foggalong/hardcode-fixer/master"
