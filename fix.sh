c5931f58eadeadbb2005ac13bede2d8196edd938
Nathanel Titane <nathanel.titane@gmail.com>
2015-05-25 17:12:23 -0400

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -31,66 +31,66 @@ general_error() { sleep 3; exit 1 }
 # Deals with option flags
 while (($#)) # Loop test handles multiple flags, no matter what the order
 do
-		if [[ -z "$1" ]]
-		then
-			mode="fix"
-		else
-			case "$1" in
-				-l|--local)
-					mode="local";;
-				-r|--revert)
-					echo "This will undo all changes previously made."
-					while true
-					do
-						read -p "Are you sure you want to continue? " answer
-						case "$answer" in
-							[yY]* ) mode="revert"; break;;
-							[nN]* ) exit;;
-							* ) echo "Please answer [Y/y]es or [N/n]o.";;
-						esac
-					done;;
-				-h|--help)
-					echo -e \
-						"Usage: ./$script [OPTION]\n" \
-						"\rFixes hardcoded icons of installed applications.\n" \
-						"\rCurrently supported options:\n" \
-						"\r  -l, --local \t Only fixes local launchers.\n" \
-						"\r  -r, --revert \t Reverts any changes made.\n" \
-						"\r  -h, --help \t\t Displays this help menu.\n" \
-						"\r  -v, --version \t Displays program version.\n"
-					exit 0 ;;
-				-v|--version)
-					echo -e "$script $date\n"
-					exit 0 ;;
-				*)
-					echo -e "$script: invalid option -- '$1'"
-					echo -e "Try '$script --help' for more information."
-					general_error
-			esac
-		fi
-	shift # Shifts through variable assignments to enable the order handlin within the loop
+        if [[ -z "$1" ]]
+        then
+            mode="fix"
+        else
+            case "$1" in
+                -l|--local)
+                    mode="local";;
+                -r|--revert)
+                    echo "This will undo all changes previously made."
+                    while true
+                    do
+                        read -p "Are you sure you want to continue? " answer
+                        case "$answer" in
+                            [yY]* ) mode="revert"; break;;
+                            [nN]* ) exit;;
+                            * ) echo "Please answer [Y/y]es or [N/n]o.";;
+                        esac
+                    done;;
+                -h|--help)
+                    echo -e \
+                        "Usage: ./$script [OPTION]\n" \
+                        "\rFixes hardcoded icons of installed applications.\n" \
+                        "\rCurrently supported options:\n" \
+                        "\r  -l, --local \t Only fixes local launchers.\n" \
+                        "\r  -r, --revert \t Reverts any changes made.\n" \
+                        "\r  -h, --help \t\t Displays this help menu.\n" \
+                        "\r  -v, --version \t Displays program version.\n"
+                    exit 0 ;;
+                -v|--version)
+                    echo -e "$script $date\n"
+                    exit 0 ;;
+                *)
+                    echo -e "$script: invalid option -- '$1'"
+                    echo -e "Try '$script --help' for more information."
+                    general_error
+            esac
+        fi
+    shift # Shifts through variable assignments to enable the order handlin within the loop
 done
 
 # Verifies if 'curl' is installed
 if ! type "curl" >> /dev/null 2>&1
 then
-	echo -e \
-		"$0: This script requires 'curl' to be installed\n" \
-		"\rto fetch the required files and check for updates.\n" \
-		"\rPlease install it and rerun this script."
-	general_error
+    echo -e \
+        "$0: This script requires 'curl' to be installed\n" \
+        "\rto fetch the required files and check for updates.\n" \
+        "\rPlease install it and rerun this script."
+    general_error
 fi
 
 # Verifies internet access
 if eval "curl -sk https://github.com/" >> /dev/null 2>&1
 then
-	: # pass
+    : # pass
 else
-	echo -e \
-		"No internet connection available. This script\n" \
-		"\rrequires internet access to connect to GitHub\n" \
-		"\rto check for updates and download 'to-fix' info."
-	general_error
+    echo -e \
+        "No internet connection available. This script\n" \
+        "\rrequires internet access to connect to GitHub\n" \
+        "\rto check for updates and download 'to-fix' info."
+    general_error
 fi
 
 # Check for newer version of the script
@@ -98,21 +98,21 @@ new_date=$(curl -sk "${git_locate}"/fix.sh | grep "date=[0-9]\{9\}" | sed "s/[^0
 
 if [[ "$date" -lt "$new_date" ]]
 then
-	echo -e \
-		"You're running an out of date version of\n" \
-		"\rthe script. Please download the latest\n" \
-		"\rverison from the GitHub page or update\n" \
-		"\rvia your package manager. If you continue\n" \
-		"\rwithout updating you may run into problems."
-	while true
-	do
-		read -p "Would you like to [e]xit, or [c]ontinue?" answer
-		case "$answer" in
-			[eE]* ) exit;;
-			[cC]* ) break;;
-			* ) echo "Please answer [e]xit or [c]ontinue";;
-		esac
-	done
+    echo -e \
+        "You're running an out of date version of\n" \
+        "\rthe script. Please download the latest\n" \
+        "\rverison from the GitHub page or update\n" \
+        "\rvia your package manager. If you continue\n" \
+        "\rwithout updating you may run into problems."
+    while true
+    do
+        read -p "Would you like to [e]xit, or [c]ontinue?" answer
+        case "$answer" in
+            [eE]* ) exit;;
+            [cC]* ) break;;
+            * ) echo "Please answer [e]xit or [c]ontinue";;
+        esac
+    done
 fi
 
 # Downloads latest version of the list
@@ -123,122 +123,122 @@ chown "$user" "/tmp/tofix.csv"
 # Verifies for root access priviledges
 if [[ "$UID" -ne 0 ]] && [[ "$mode" != "local" ]]
 then
-	echo "The script must be run as root to (un)fix global launchers."
-	while true
+    echo "The script must be run as root to (un)fix global launchers."
+    while true
 do
-	read -p "Do you want to continue in local mode? " answer
-	case "$answer" in
-		[yY]* )
-			if [[ "$mode" == "fix" ]]
-			then
-				mode="local"; break
-			elif [[ "$mode" == "revert" ]]
-			then
-				mode="l-revert"; break
-			fi;;
-		[nN]* ) exit;;
-		* ) echo "Please answer [Y/y]es or [N/n]o.";;
-		esac
-	done
+    read -p "Do you want to continue in local mode? " answer
+    case "$answer" in
+        [yY]* )
+            if [[ "$mode" == "fix" ]]
+            then
+                mode="local"; break
+            elif [[ "$mode" == "revert" ]]
+            then
+                mode="l-revert"; break
+            fi;;
+        [nN]* ) exit;;
+        * ) echo "Please answer [Y/y]es or [N/n]o.";;
+        esac
+    done
 fi
 
 # Iterating over lines of tofix.csv, each split into an array
 IFS=","
 while read -r name launcher current new_icon
 do
-	# Basic corrections
-	name=$(echo "$name" | sed -e "s/\r//g")
-	launcher=$(echo "$launcher".desktop | sed -e "s/\r//g")
-	current=$(echo "$current" | sed -e "s/\r//g")
-	new_icon=$(echo "$new_icon" | sed -e "s/\r//g")
-	# Escape non-standard and special characters in file names by creating a new variable
-	old_icon="${current//\\/\\\\}" # escape backslashes
-	old_icon="${old_icon//\//\\/}" # escape slashes
+    # Basic corrections
+    name=$(echo "$name" | sed -e "s/\r//g")
+    launcher=$(echo "$launcher".desktop | sed -e "s/\r//g")
+    current=$(echo "$current" | sed -e "s/\r//g")
+    new_icon=$(echo "$new_icon" | sed -e "s/\r//g")
+    # Escape non-standard and special characters in file names by creating a new variable
+    old_icon="${current//\\/\\\\}" # escape backslashes
+    old_icon="${old_icon//\//\\/}" # escape slashes
 
-	# Fixing code
-	if [[ "$mode" == "fix" ]] || [[ "$mode" == "local" ]]
-	then
-		# Local & Steam launchers
-		if [[ -f "$local_apps$launcher" ]]
-		then
-			if [[ "$current" != "steam" ]]
-			then
-				# Local launchers
-				if [[ -f "$current" ]]
-				then # Verifies if icon exists to copy
-					if grep -Gq "Icon=$current$" "$local_apps$launcher"
-					then
-						echo "L: Fixing $name..."
-						if [[ ! -d "$local_icon" ]]
-						then
-							su -c "mkdir '$local_icon' -p" "$user"
-						fi
-						cp "$current" "$local_icon$new_icon"
-						sed -i "s/Icon=${old_icon}.*/Icon=$new_icon/" "$local_apps$launcher"
-					fi
-				fi
-			else
-				# Steam launchers
-				if [[ -f "$steam_icon" ]]
-				then # Verifies if steam icon exists to copy
-					if grep -Gq "Icon=$current$" "$local_apps$launcher"
-					then
-						echo "S: Fixing $name..."
-						if [[ ! -d "$local_icon" ]]
-						then
-							su -c "mkdir '$local_icon' -p" "$user"
-						fi
-						cp "$steam_icon" "$local_icon${new_icon}.png"
-						sed -i "s/Icon=steam.*/Icon=$new_icon/" "$local_apps$launcher"
-					fi
-				fi
-			fi
-		fi
-		# Global launchers
-		if [[ $mode != "local" ]] && [[ -f "$global_apps$launcher" ]]
-		then
-			if [[ -f "$current" ]]
-			then # Verifies if icon exists to copy
-				if grep -Gq "Icon=$current$" "$global_apps$launcher"
-				then
-					echo "G: Fixing $name..."
-					cp "$current" "$global_icon$new_icon"
-					sed -i "s/Icon=${old_icon}.*/Icon=$new_icon/" "$global_apps$launcher"
-				fi
-			fi
-		fi
-	# Reversion code
-	elif [[ "$mode" == "revert" ]] || [[ "$mode" == "l-revert" ]]
-	then
-		# Local revert
-		if [[ -f "$local_apps$launcher" ]] && [[ -f "$current" ]]
-		then
-			if grep -Gq "Icon=$new_icon$" "$local_apps$launcher"
-			then
-				echo "F: Reverting $name..."
-				rm -f "$local_icon$new_icon"*
-				sed -i "s/Icon=${new_icon}.*/Icon=$old_icon/" "$local_apps$launcher"
-			fi
-		fi
-		# Steam revert
-		if [[ -f "$local_apps$launcher" ]] && [[ -f "$steam_icon" ]]
-		then
-			if grep -Gq "Icon=$new_icon$" "$local_apps$launcher"
-			then
-				echo "S: Reverting $name..."
-				rm -f "$local_icon$new_icon"*
-				sed -i "s/Icon=${new_icon}.*/Icon=$old_icon/" "$local_apps$launcher"
-			fi
-		fi
-		# Global revert
-		if [[ $mode != "l-revert" ]] && [[ -f "$global_apps$launcher" ]] && [[ -f "$current" ]]
-		then
-			if grep -Gq "Icon=$new_icon$" "$global_apps$launcher"
-			then
-				echo "G: Reverting $name..."
-				rm -f "$global_icon$new_icon"*
-				sed -i "s/Icon=${new_icon}.*/Icon=$old_icon/" "$global_apps$launcher"
-			fi
-		fi
-	fi
+    # Fixing code
+    if [[ "$mode" == "fix" ]] || [[ "$mode" == "local" ]]
+    then
+        # Local & Steam launchers
+        if [[ -f "$local_apps$launcher" ]]
+        then
+            if [[ "$current" != "steam" ]]
+            then
+                # Local launchers
+                if [[ -f "$current" ]]
+                then # Verifies if icon exists to copy
+                    if grep -Gq "Icon=$current$" "$local_apps$launcher"
+                    then
+                        echo "L: Fixing $name..."
+                        if [[ ! -d "$local_icon" ]]
+                        then
+                            su -c "mkdir '$local_icon' -p" "$user"
+                        fi
+                        cp "$current" "$local_icon$new_icon"
+                        sed -i "s/Icon=${old_icon}.*/Icon=$new_icon/" "$local_apps$launcher"
+                    fi
+                fi
+            else
+                # Steam launchers
+                if [[ -f "$steam_icon" ]]
+                then # Verifies if steam icon exists to copy
+                    if grep -Gq "Icon=$current$" "$local_apps$launcher"
+                    then
+                        echo "S: Fixing $name..."
+                        if [[ ! -d "$local_icon" ]]
+                        then
+                            su -c "mkdir '$local_icon' -p" "$user"
+                        fi
+                        cp "$steam_icon" "$local_icon${new_icon}.png"
+                        sed -i "s/Icon=steam.*/Icon=$new_icon/" "$local_apps$launcher"
+                    fi
+                fi
+            fi
+        fi
+        # Global launchers
+        if [[ $mode != "local" ]] && [[ -f "$global_apps$launcher" ]]
+        then
+            if [[ -f "$current" ]]
+            then # Verifies if icon exists to copy
+                if grep -Gq "Icon=$current$" "$global_apps$launcher"
+                then
+                    echo "G: Fixing $name..."
+                    cp "$current" "$global_icon$new_icon"
+                    sed -i "s/Icon=${old_icon}.*/Icon=$new_icon/" "$global_apps$launcher"
+                fi
+            fi
+        fi
+    # Reversion code
+    elif [[ "$mode" == "revert" ]] || [[ "$mode" == "l-revert" ]]
+    then
+        # Local revert
+        if [[ -f "$local_apps$launcher" ]] && [[ -f "$current" ]]
+        then
+            if grep -Gq "Icon=$new_icon$" "$local_apps$launcher"
+            then
+                echo "F: Reverting $name..."
+                rm -f "$local_icon$new_icon"*
+                sed -i "s/Icon=${new_icon}.*/Icon=$old_icon/" "$local_apps$launcher"
+            fi
+        fi
+        # Steam revert
+        if [[ -f "$local_apps$launcher" ]] && [[ -f "$steam_icon" ]]
+        then
+            if grep -Gq "Icon=$new_icon$" "$local_apps$launcher"
+            then
+                echo "S: Reverting $name..."
+                rm -f "$local_icon$new_icon"*
+                sed -i "s/Icon=${new_icon}.*/Icon=$old_icon/" "$local_apps$launcher"
+            fi
+        fi
+        # Global revert
+        if [[ $mode != "l-revert" ]] && [[ -f "$global_apps$launcher" ]] && [[ -f "$current" ]]
+        then
+            if grep -Gq "Icon=$new_icon$" "$global_apps$launcher"
+            then
+                echo "G: Reverting $name..."
+                rm -f "$global_icon$new_icon"*
+                sed -i "s/Icon=${new_icon}.*/Icon=$old_icon/" "$global_apps$launcher"
+            fi
+        fi
+    fi
 done < "/tmp/tofix.csv"
